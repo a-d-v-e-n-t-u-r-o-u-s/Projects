7955049c5a61278a2539245158c455dd3d5d3fc8
Dawid Babula <dbabula@adventurous.pl>
2016-01-23 09:22:42 -0800

    SCRIPTS: modify enviroment variable setup script

--- scripts/setenv.bat
+++ scripts/setenv.bat
@@ -1,3 +1,3 @@
-set PATH=C:\Windows\system32;C:\Program Files\COSMIC\32K_Compilers\CXSTM8;C:\Program Files\make-3.81-bin\bin;C:\Program Files\Vim;C:\Program Files\STMicroelectronics\st_toolset\stvp
+set PATH=C:\Windows\system32;C:\Program Files\COSMIC\32K_Compilers\CXSTM8;C:\Program Files\make-3.81-bin\bin;C:\Program Files\Vim;C:\Program Files\STMicroelectronics\st_toolset\stvp;c:\Program Files\doxygen\bin\;c:\Program Files\Graphviz2.38\bin\
 
 %comspec% /k PATH
