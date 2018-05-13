28097f972927edce4dc8e907da485d210a2b6f17
Dawid Babula <dbabula@adventurous.pl>
2018-05-13 17:42:59 +0200

    MAIN: configure debug and display banner

--- modules/RemoteControl/source/main.c
+++ modules/RemoteControl/source/main.c
@@ -3,59 +3,70 @@
 #include "system.h"
 #include "system_timer.h"
 #include "display.h"
+#include "debug.h"
+#include <stdio.h>
 
 #define LED_GREEN_PORT              GPIOA
 #define LED_GREEN_PIN               GPIO_Pin_7
-#define LED_BLUE_PORT               GPIOC
-#define LED_BLUE_PIN                GPIO_Pin_7
 
 INTERRUPT_HANDLER(SYSTEM_clock_switch, 17);
+INTERRUPT_HANDLER(DEBUG_usart2_tx, 19);
 INTERRUPT_HANDLER(SYSTEM_timer_tick, 25);
 
 static INLINE_SPECIFIER void drivers_init(void)
 {
-/*
- *    DISP_config_t config =
- *    {
- *        .function = (uint8_t) (DISP_MODE_4_BIT|DISP_FONTS5x7|DISP_TWO_LINE),
- *        .control = (uint8_t)( DISPLAY_ON| CURSOR_ON | CURSOR_BLINK),
- *        .mode = (uint8_t)( EM_SHIFT_CURSOR | EM_INCREMENT),
- *        .cols = 16,
- *        .rows = 2
- *    };
- *
- *    DISP_configure(&config);
- */
+    DEBUG_config_t config =
+    {
+        .unit = USART2,
+        .clk = CLK_Peripheral_USART2,
+        .pullup_port = GPIOE,
+        .pullup_pin = 4U,
+        .baudrate = 115200U,
+        .word_length = USART_WordLength_8b,
+        .stop_bits = USART_StopBits_1,
+        .parity = USART_Parity_No,
+        .mode = USART_Mode_Tx,
+    };
+
+    DEBUG_configure(&config);
 }
 
 static INLINE_SPECIFIER void modules_init(void)
 {
 }
 
+static void callback(void)
+{
+}
+
 int main(void)
 {
-    //GPIO_Init(LED_BLUE_PORT,LED_BLUE_PIN,GPIO_Mode_Out_PP_High_Fast);
     GPIO_Init(LED_GREEN_PORT,LED_GREEN_PIN,GPIO_Mode_Out_PP_High_Fast);
 
-    //SYSTEM_init();
+    SYSTEM_init();
+    SYSTEM_timer_register(callback);
 
     drivers_init();
     modules_init();
 
-    //DISP_send_text(0,0,"Hello world !!!");
+    for(uint8_t i = 0; i < 10; i++)
+    {
+        SYSTEM_timer_delay(1);
+        GPIO_ToggleBits(LED_GREEN_PORT,LED_GREEN_PIN);
+    }
+
+    SYSTEM_timer_delay(100);
+    GPIO_ToggleBits(LED_GREEN_PORT,LED_GREEN_PIN);
+
+    printf("********************************\n");
+    printf("******* Remote Control *********\n");
+    printf("********************************\n");
 
     while(1)
     {
-        /*
-         *SYSTEM_main();
-         *SYSTEM_timer_delay(100);
-         *GPIO_ToggleBits(LED_BLUE_PORT,LED_BLUE_PIN);
-         *GPIO_ToggleBits(LED_GREEN_PORT,LED_GREEN_PIN);
-         *SYSTEM_timer_delay(100);
-         */
-        uint32_t i = 10000;
+        SYSTEM_main();
 
-        while(i--);
         GPIO_ToggleBits(LED_GREEN_PORT,LED_GREEN_PIN);
+        SYSTEM_timer_delay(100);
     }
 }
