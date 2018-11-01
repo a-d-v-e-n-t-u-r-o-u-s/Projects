f857f6155f88eef9900b8bf4ace86eb919b903dd
Dawid Babula <dbabula@adventurous.pl>
2018-11-01 21:40:17 +0100

    SYSTEM: copy stm8 system to separate files

---
+++ modules/System/source/system_stm8.c
@@ -0,0 +1,72 @@
+#include "system.h"
+#include "stm8l15x_clk.h"
+#include "system_timer.h"
+#include "system_common.h"
+
+#define SYSTEM_MAX_TASKS    5
+
+/*!
+ * \todo HSE clock on pin might be necessary
+ */
+
+typedef struct
+{
+    SYSTEM_task_t task;
+} task_data_t;
+
+static task_data_t tasks[SYSTEM_MAX_TASKS];
+static uint8_t task_counter;
+
+INTERRUPT_HANDLER(SYSTEM_clock_switch, 17)
+{
+    CLK_ClearITPendingBit(CLK_IT_SWIF);
+}
+
+int8_t SYSTEM_register_task(SYSTEM_task_t task)
+{
+    uint8_t i=0;
+
+    if(task == NULL || task_counter == SYSTEM_MAX_TASKS)
+    {
+        return -1;
+    }
+
+    tasks[task_counter].task = task;
+    task_counter++;
+    return 0;
+}
+
+void SYSTEM_main(void)
+{
+    uint8_t i = 0;
+
+    for(i=0;i<task_counter;i++)
+    {
+        if(tasks[i].task != NULL)
+        {
+            tasks[i].task();
+        }
+    }
+}
+
+void SYSTEM_init(void)
+{
+    CLK_DeInit();
+
+    CLK_SYSCLKSourceSwitchCmd(ENABLE);
+    CLK_ITConfig(CLK_IT_SWIF,ENABLE);
+#if 0
+    /*!
+     * \todo HSI clock set for the RemoteControl project,
+     * for WeatherStation should be change to HSE
+     */
+    CLK_SYSCLKSourceConfig(CLK_SYSCLKSource_HSE);
+    CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
+#else
+    CLK_SYSCLKSourceConfig(CLK_SYSCLKSource_HSI);
+    CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_2);
+#endif
+    CLK_PeripheralClockConfig(CLK_Peripheral_BOOTROM,DISABLE);
+
+    SYSTEM_timer_init();
+}
