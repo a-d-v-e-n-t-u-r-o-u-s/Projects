10cd56cc48a5d35861c0ee5b4991fdb724caa80e
Dawid Babula <dbabula@adventurous.pl>
2018-11-01 21:15:30 +0100

    FIFO: yet another fix of FIFO function prototypes

--- drivers/common/include/fifo.h
+++ drivers/common/include/fifo.h
@@ -54,7 +54,7 @@ typedef struct
 } FIFO_t;
 
 int8_t FIFO_enqueue(FIFO_t *fifo, const void *data);
-int8_t FIFO_dequeue(FIFO_t *fifo, const void *data);
+int8_t FIFO_dequeue(FIFO_t *fifo, void *data);
 FIFO_elements_no_t FIFO_get_free(FIFO_t *fifo);
 FIFO_elements_no_t FIFO_get_occupied(FIFO_t *fifo);
 void FIFO_configure(FIFO_t *fifo, const FIFO_config_t *config);
