b9ff58dbbc1878d0dd2647b3beced0e9e851479f
Dawid Babula <dbabula@adventurous.pl>
2018-11-04 16:26:41 +0100

    BUILD: fix wrong linking of application

--- config/compiler-avr-gcc.mk
+++ config/compiler-avr-gcc.mk
@@ -1,12 +1,14 @@
 CC = avr-gcc
-LD = avr-ld
+LD = avr-gcc
 AR = avr-ar
 LOADER = avrdude
 
 LIBPREFIX := lib
 LIBEXT := .a
 
-#CDEFS =
+CDEFS = F_CPU=8000000UL
+
+ARFLAGS = rcs
 
 CFLAGS = -c
 CFLAGS += -Wall
@@ -17,8 +19,11 @@ CFLAGS += $(addprefix -I,$(INCLUDE_DIR))
 CFLAGS += $(addprefix -D,$(CDEFS))
 CFLAGS += -o $(OBJECTS_DIR)/$(@F)
 
-#LDFLAGS = -mmcpu=avr4
+LDFLAGS += -mmcu=atmega8
+LDFLAGS += -Wl,-Map,$(BIN_DIR_FORMATED)$(DELIM)$(OUTPUT).map
+LDFLAGS += -Wl,--cref
+LDFLAGS += -Wl,-L$(LIB_DIR)
 
 LOADER_FLAGS += -c usbasp
 LOADER_FLAGS += -p m8
-LOADER_FLAGS += -U flash:w:$(BIN_DIR_FORMATED)$(DELIM)$(OUTPUT).hex
+LOADER_FLAGS += -U flash:w:$(BIN_DIR_FORMATED)$(DELIM)$(OUTPUT).elf
--- modules/MiniThermometer/source/main.c
+++ modules/MiniThermometer/source/main.c
@@ -22,8 +22,6 @@
  */
 #include <stdint.h>
 
-#define F_CPU 8000000UL
-
 #include <avr/io.h>
 #include <util/delay.h>
 #include "system.h"
