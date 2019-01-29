60a35d8378045521bf8e094a6a010e79e0b3b009
Dawid Babula <dbabula@adventurous.pl>
2019-01-29 23:32:45 +0100

    MINITHERMO: enable debug driver

--- projects/MiniThermometer/drivers.mk
+++ projects/MiniThermometer/drivers.mk
@@ -2,6 +2,6 @@ USED_DRIVERS += common
 USED_DRIVERS += usart
 #USED_DRIVERS += display
 #USED_DRIVERS += pressure
-#USED_DRIVERS += debug
+USED_DRIVERS += debug
 #USED_DRIVERS += pwm
 #USED_DRIVERS += rtc
